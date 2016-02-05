0b1383091e3ff4a4d42923f98130c23107743753
Daniel Pradera <Daniel Pradera>
2016-02-05 12:52:57 +0100

    Añadido el constructor de la clase Jugador y getter para el id

---
+++ Jugador.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Jugador here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Jugador
+{
+    // instance variables - replace the example below with your own
+    private String id;
+
+    /**
+     * Constructor for objects of class Jugador
+     */
+    public Jugador(String id)
+    {
+        this.id = id;
+    }
+    
+    /**
+     * Devuelve el identificador (id) del jugador
+     */
+    private String getId()
+    {
+        return id;
+    }
+}
